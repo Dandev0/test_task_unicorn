b2a1d8a0812c1b7adf3d57d70e61fedbe7e95966
Dandev0 <99640203+Dandev0@users.noreply.github.com>
2022-09-24 16:55:58 +0500

    Update Abstractclass.py

--- Abstractclass.py
+++ Abstractclass.py
@@ -1,5 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class AbstractBase(ABC):
-    pass
 
+    @abstractmethod
+    async def take_valute(self):
+        """Раз в -period получает данные о курсе валют(-period задает пользователь при запуске микросервиса)"""
+
+    @abstractmethod
+    async def save_update_balance(self):
+        """Сохраняет обновленный баланс"""
+
+    @abstractmethod
+    async def save_valute(self):
+        """Сохраняем валюту, чтобы в последствии производить сравнение и выводить информацию в консоль при ее изменении"""
+
+    @abstractmethod
+    async def sum_valute(self):
+        """Производим конвертацию баланса в кошелька в каждую из 3 валют"""
